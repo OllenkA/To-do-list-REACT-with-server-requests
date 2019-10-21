b455c3029bb069634b88fa7545138764595ec806
Ollenka <Ollenka@gmail.com>
2019-10-21 11:35:24 +0300

    Updates

---
+++ precache-manifest.007487e323ddb521da3dc4ddc30ab7ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2658d5e39c4beaf9c85d88bd70e8e9d3",
+    "url": "/To-do-list-REACT-with-server-requests/index.html"
+  },
+  {
+    "revision": "1114584652cbe4e6dbc1",
+    "url": "/To-do-list-REACT-with-server-requests/static/css/main.4eced717.chunk.css"
+  },
+  {
+    "revision": "1fda0879dc2f4281f1e5",
+    "url": "/To-do-list-REACT-with-server-requests/static/js/2.7b46c36c.chunk.js"
+  },
+  {
+    "revision": "1114584652cbe4e6dbc1",
+    "url": "/To-do-list-REACT-with-server-requests/static/js/main.65281b3c.chunk.js"
+  },
+  {
+    "revision": "51963fb9f22ca38ff941",
+    "url": "/To-do-list-REACT-with-server-requests/static/js/runtime~main.92ded889.js"
+  },
+  {
+    "revision": "fec27d6ec0f1705dae1fbe29eda61472",
+    "url": "/To-do-list-REACT-with-server-requests/static/media/back-navigational-arrow-button-pointing-to-left-svgrepo-com.fec27d6e.svg"
+  },
+  {
+    "revision": "d4798137877b0297bc4e25c161c7f101",
+    "url": "/To-do-list-REACT-with-server-requests/static/media/delete-button.d4798137.svg"
+  },
+  {
+    "revision": "c47c123552bbe7334988c81d31c19e3e",
+    "url": "/To-do-list-REACT-with-server-requests/static/media/mail_ray_stuff_regular.c47c1235.ttf"
+  }
+]);
